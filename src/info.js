95a39b521a968084c7b882812ba325319279751b
Mehul <318097@gmail.com>
2019-10-22 18:48:44 +0530

    Updated: Info file

--- src/info.js
+++ src/info.js
@@ -1,138 +1,222 @@
 exports.profile = {
-  basic: {
-    name: 'Mehul Lakhanpal',
-    dob: '09-11-1993',
-    profession: 'Web Developer',
-    email: '318097@gmail.com',
-    mobile: 7760279446,
-    profile_image: ['imagename.jpg'],
-    address_1: '#D3, Haines Road,',
-    address_2: 'Frazer Town,',
-    city: 'Bangalore',
-    state: 'Karnataka',
-    pincode: 560005,
-    website: 'www.brainbox.in'
+  "basic": {
+    "name": "Mehul Lakhanpal",
+    "dob": "09-11-1993",
+    "profession": "Full stack developer",
+    "email": "318097@gmail.com",
+    "mobile": "7760279446",
+    "profile_image": [
+      "profile.jpg"
+    ],
+    "address_1": "#46 Haines Road",
+    "address_2": "Frazer Town",
+    "city": "Bangalore",
+    "state": "Karnataka",
+    "pincode": "560005",
+    "website": "www.brainbox.in"
   },
-  education: [
+  "education": [
     {
-      course: '10th Standard',
-      university: 'Indian Certificate of Secondary Examination (ICSE)',
-      score: '80%',
-      name: 'St. Johns High School',
-      graduation_year: 2009
+      "course": "10th Standard",
+      "university": "Indian Certificate of Secondary Examination (ICSE)",
+      "score": "80%",
+      "name": "St. Johns High School",
+      "graduation_year": 2009
     },
     {
-      course: '12th Standard',
-      university: 'Pre University Council (PUC)',
-      score: '56.4%',
-      name: 'Sri Bhagwan Mahaveer Jain College',
-      graduation_year: 2011
+      "course": "12th Standard",
+      "university": "Pre University Council (PUC)",
+      "score": "56.4%",
+      "name": "Sri Bhagwan Mahaveer Jain College",
+      "graduation_year": 2011
     },
     {
-      course: 'Bachelor\'s in Computer Science Engineering (CSE)',
-      university: 'Visvesvaraya Technological University, Belgavi (VTU)',
-      score: '52.1%',
-      name: 'T John Institute of Technology',
-      graduation_year: 2016
+      "course": "Bachelor in Engineering (CSE)",
+      "university": "Visvesvaraya Technological University, Belgavi (VTU)",
+      "score": "52.1%",
+      "name": "T John Institute of Technology",
+      "graduation_year": 2016
     }
   ],
-  work: [
+  "work": [
     {
-      name: 'Pesto Career Accelerator',
-      role: 'Student',
-      start_date: 'Feb \'19',
-      end_date: 'Apr \'19',
-      description: "Pesto is a 12-week intensive full-stack developer training program for India’s top software engineers.",
-      projects: []
+      "name": "Clipboard Health",
+      "role": "Full stack developer",
+      "start_date": "17-06-2019",
+      "end_date": "04-10-2019",
+      "projects": [
+        {}
+      ]
     },
     {
-      name: 'Ideaplunge Solutions',
-      role: 'Application developer',
-      start_date: 'Feb \'18',
-      end_date: 'Dec \'18',
-      description: "A software development consultancy.",
-      projects: [
+      "name": "Ideaplunge Solutions",
+      "role": "Front end developer",
+      "start_date": "01-02-2018",
+      "end_date": "01-01-2019",
+      "projects": [
         {
-          name: 'Center for Digital Financial Inclusion (CDFI)',
-          description: ['Created a custom drill-down feature in amCharts to automatically create new charts by clicking on sections of an existing chart.'],
+          "name": "Sankalp",
+          "description": [
+            "Created a custom drill-down feature in amCharts to automatically create new charts by clicking on sections of an existing chart."
+          ],
+          "stack": [
+            "Mongodb",
+            "Nodejs",
+            "Angular"
+          ]
         },
         {
-          name: 'Essilor & Shimazdu',
-          description: [
-            'Developed a custom CMS application in Angular 2+ that allowed us to rebuild a complex frontend by making changes to a backend JSON file. This included dynamically rendering forms and accepting that data via a REST API.',
-            'Designed the CMS to be modular so that it could be re-used across two of our different clients, Essilor and Shimazdu.'
+          "name": "Essilor & Shimadzu",
+          "description": [
+            "Developed a custom CMS application in Angular 2+ that allowed us to rebuild a complex frontend by making changes to a backend JSON file. This included dynamically rendering forms and accepting that data via a REST API.",
+            "Designed the CMS to be modular so that it could be re - used across two of our different clients, Essilor and Shimazdu."
           ],
+          "stack": [
+            "Mongodb",
+            "Nodejs",
+            "Angular"
+          ]
         },
         {
-          name: 'P&G Internal Report Management System',
-          description: [
-            'Implemented cross-browser compatibility (IE, Firefox, etc) and responsive design fixes.',
-            'Implemented data management and fetching for reports using @ngrx.'
+          "name": "P&G Reports",
+          "description": [
+            "Implemented cross-browser compatibility (IE, Firefox, etc) and responsive design fixes.",
+            "Implemented data management and fetching for reports using @ngrx."
           ],
+          "stack": [
+            "Mongodb",
+            "Nodejs",
+            "Angular"
+          ]
         },
         {
-          name: 'CPS',
-          description: [
-            'Finished remaining backend REST API endpoints and refactored existing code to be more maintainable and readable.'
+          "name": "CPS",
+          "description": [
+            "Finished remaining backend REST API endpoints and refactored existing code to be more maintainable and readable."
           ],
+          "stack": [
+            "Mongodb",
+            "Nodejs",
+            "Angular"
+          ]
         }
       ]
     },
     {
-      name: 'Net Analytiks',
-      role: 'Full stack developer',
-      start_date: 'Aug \'17',
-      end_date: 'Dec \'17',
-      description: "A software development consultancy.",
-      projects: [
+      "name": "Net Analytiks",
+      "role": "Full stack developer",
+      "start_date": "01-08-2017",
+      "end_date": "15-12-2017",
+      "projects": [
         {
-          name: 'iWish bids',
-          description: [
-            'Developed REST APIs endpoints',
-            'Worked on the responsiveness of the application',
-            'Built a complex frontend using vanilla Javascript'
+          "name": "iWish",
+          "description": [
+            "Developed REST APIs endpoints",
+            "Worked on the responsiveness of the application",
+            "Built a complex frontend using vanilla Javascript"
           ],
+          "stack": [
+            "Mongodb",
+            "Nodejs",
+            "Javascript"
+          ]
         }
       ]
     }
   ],
-  skills: [
-    { name: 'Javascript' },
-    { name: 'Angular 2+' },
-    { name: 'Angular Material' },
-    { name: 'GIT' },
-    { name: 'HTML' },
-    { name: 'CSS' },
-    { name: 'Node.js' },
-    { name: 'Express.js' },
-    { name: 'Mongo DB' },
-    { name: 'SQL' },
-    { name: 'RxJS' },
-    { name: 'TypeScript' },
-    { name: 'React' },
-    { name: 'PHP' }
+  "skills": [
+    {
+      "name": "HTML"
+    },
+    {
+      "name": "CSS"
+    },
+    {
+      "name": "Javascript"
+    },
+    {
+      "name": "React"
+    },
+    {
+      "name": "Ant Design"
+    },
+    {
+      "name": "Redux"
+    },
+    {
+      "name": "Node.js"
+    },
+    {
+      "name": "Express.js"
+    },
+    {
+      "name": "Mongo DB"
+    },
+    {
+      "name": "Angular"
+    },
+    {
+      "name": "Angular Material"
+    },
+    {
+      "name": "PHP"
+    }
   ],
-  secondary_skills: [
-    { name: 'C' },
-    { name: 'Java' },
-    { name: 'amCharts' },
-    { name: 'Heroku' },
-    { name: 'AngularJS' },
-    { name: 'Gulp' },
-    { name: 'Bootstrap' },
-    { name: 'Socket.IO' },
-    { name: 'ngrx' },
-    { name: 'Redux' },
-    { name: 'AJAX' },
-    { name: 'SASS' },
-    { name: 'JEST' }
+  "secondary_skills": [
+    {
+      "name": "C"
+    },
+    {
+      "name": "Firebase"
+    },
+    {
+      "name": "Heroku"
+    },
+    {
+      "name": "Typescript"
+    },
+    {
+      "name": "Socket.IO"
+    },
+    {
+      "name": "AJAX"
+    },
+    {
+      "name": "SASS"
+    },
+    {
+      "name": "Jest"
+    },
+    {
+      "name": "GIT"
+    },
+    {
+      "name": "RxJS"
+    },
+    {
+      "name": "SQL"
+    }
   ],
-  social: [
-    { name: 'GITHUB', url: 'https: //github.com/318097' },
-    { name: 'FACEBOOK', url: 'https: //www.facebook.com/ml.cr7' },
-    { name: 'LINKEDIN', url: 'https: //www.linkedin.com/in/318097/' },
-    { name: 'HACKEREARTH', url: 'https: //www.hackerearth.com/@318097' }
+  "social": [
+    {
+      "name": "GITHUB",
+      "url": "https: //github.com/318097"
+    },
+    {
+      "name": "FACEBOOK",
+      "url": "https: //www.facebook.com/ml.cr7"
+    },
+    {
+      "name": "LINKEDIN",
+      "url": "https: //www.linkedin.com/in/318097/"
+    },
+    {
+      "name": "HACKEREARTH",
+      "url": "https: //www.hackerearth.com/@318097"
+    }
   ],
-  personal_projects: [{}],
-  resume: 'url'
+  "personal_projects": [
+    {}
+  ],
+  "resume": "url"
 };
