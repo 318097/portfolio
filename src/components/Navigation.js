b2e065cfe21b6896abaf42cca4d8d5fafe00852c
Mehul Lakhanpal <318097@gmail.com>
2020-04-08 19:50:26 +0530

    Refactor

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,20 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.scss";
 
-const navItems = [
-  { id: 1, name: "profile" },
-  { id: 2, name: "work" },
-  { id: 3, name: "skills" },
-  { id: 4, name: "contact" },
-];
+const navItems = ["profile", "work", "skills", "contact"];
 
 const Navigation = ({ activeSection }) => (
   <nav>
     {navItems.map((item) => (
       <Link
-        key={item.id}
-        className={`item ${activeSection === item.name ? "active-link" : ""}`}
-        to={`#${item.name}`}
+        key={item}
+        className={`item ${activeSection === item ? "active-link" : ""}`}
+        to={`#${item}`}
       >
         {item.name.toUpperCase()}
       </Link>
